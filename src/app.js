84b4bbb21ede11c68bb4e2b686f4cd7648fed855
khushi gupta <kg834208@gmail.com>
2025-11-04 23:22:22 +0530

    create first post api call

--- src/app.js
+++ src/app.js
@@ -1,16 +1,28 @@
 require("dotenv").config();
-
-const express = require('express');
-const {connectDB} = require("./config/database")
+const express = require("express");
+const { connectDB } = require("./config/database");
 const app = express();
+const User = require("./models/user");
 
-connectDB().then(()=>{
-    console.log("Database connection established...")
-    app.listen(3000,()=>{
-    console.log("Server is successfully listening on port 3000")
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Khushi",
+    lastName: "Gupta",
+    emailId: "kg123@gmail.com",
+    password: "1234",
+  });
+  // Creating a new instance of the User model
+  await user.save();
+  res.send("User Added successfully")
 });
- }).catch(err =>{
-    console.error("Database cannot be connected!")
- })
 
-
+connectDB()
+  .then(() => {
+    console.log("Database connection established...");
+    app.listen(3000, () => {
+      console.log("Server is successfully listening on port 3000");
+    });
+  })
+  .catch((err) => {
+    console.error("Database cannot be connected!");
+  });
