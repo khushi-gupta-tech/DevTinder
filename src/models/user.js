752849256f0621dedcfe5c043f93dc69c219711f
khushi gupta <kg834208@gmail.com>
2025-11-04 22:34:34 +0530

    create user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
   firstName: {
     type: String,
   },
@@ -20,3 +20,7 @@ const userSchema = mongoose.Schema({
     type: String,
   },
 });
+
+const User = mongoose.model("User",userSchema)
+
+module.exports = User;
