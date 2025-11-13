2dcff462502a7ae4c87569b5b1e609b69eb22144
khushi gupta <kg834208@gmail.com>
2025-11-14 01:08:44 +0530

    make user connections api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,22 +1,46 @@
-const express = require("express")
+const express = require("express");
 const userRouter = express.Router();
-const {userAuth} = require("../middlewares/auth");
-const connectionRequest = require("../models/connectionRequest")
+const { userAuth } = require("../middlewares/auth");
+const connectionRequest = require("../models/connectionRequest");
+
+const USER_SAFE_DATA = "firstName lastName photoUrl gender age about";
+
 // Get all the pending connection request for the loggedIn user
-userRouter.get("/user/requests/received",userAuth,async(req,res)=>{
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await connectionRequest
+      .find({
+        toUserId: loggedInUser._id,
+        status: "interested",
+      })
+      .populate("fromUserId", USER_SAFE_DATA);
 
-    try{
-     const loggedInUser = req.user;
-     const connectionRequests = await connectionRequest.find({
-        toUserId:loggedInUser._id,
-        status:"interested",
-     }).populate("fromUserId",["firstName","lastName","photoUrl","gender","age","about"])
+    res.json({
+      message: "Data fetched successfully",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
 
-     res.json({message:"Data fetched successfully",
-        data:connectionRequests,
-     })
-    }catch(err){
-        res.status(400).send("ERROR: "+ err.message)
-    }
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await connectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    }).populate("fromUserId",USER_SAFE_DATA)
+
+    const data = connectionRequests.map((row)=> row.fromUserId);
+
+    res.json({ data: data });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
 });
 module.exports = userRouter;
